b432e0fd4ddc146341de3eb799e69fb6555452b2
sanrab <sanrab@users.noreply.github.com>
2018-12-12 08:21:11 +0100

    Update arduino_bmp_to_file.py

--- arduino_bmp_to_file.py
+++ arduino_bmp_to_file.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python
 import serial
 import time
+ser=serial.Serial('/dev/ttyUSB1',9600)
+while True:
+    f=open('/home/pi/bmp_data.txt','w')
+    data=ser.readline()
+    f.write(data)
+#    print data
+
+time.sleep(10)
